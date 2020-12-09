e0717fbd8fc14f4dcb414827209637b0d5dc90af
gabriel <akingdebased@gmail.com>
2020-12-08 20:29:35 -0800

    add gitignore, .env.sample...
    * add postgres libraries (pg, knex, objection)
    * edit README

---
+++ README.md
@@ -0,0 +1,11 @@
+## running locally
+
+`npm start` will spin up a server on your machine. the bot will immediately be avaible in [faithlessfew's twitch chat](https://www.twitch.tv/faithlessfew).
+
+## deloying
+
+* tbd
+
+## connecting (local SSH)
+
+the `ssh` command (on mac/unix, anyway), is `ssh -i [directoryOfPrivateKey] [username]@[publicIp]
--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
-console.log('susurrus initialized');
-
 import tmi from 'tmi.js';
 
-const BLOCKED_WORDS = ['cats'];
+import { hello, swearJar, bang} from './commands.js';
+
+const {
+	DB_HOST,
+	DB_USER,
+	DB_PASSWORD,
+	DB_NAME
+} = process.env;
+
+console.log('susurrus initialized');
+console.log({
+	DB_HOST,
+	DB_USER,
+	DB_PASSWORD,
+	DB_NAME
+});
+
+// This is the one library we'll use 'require' for, specficially to maintain parity with the knex docs
+const knex = require('knex')({
+	client: 'pg',
+	connection: {
+	  host: DB_HOST,
+	  user: DB_USER,
+	  password: DB_PASSWORD,
+	  database: DB_NAME,
+	}
+});
+
+knex.schema.createTable('users', table => { 
+	table.increments('id');
+	table.string('name');
+}).then(() => {
+	console.log('table created!');
+});
 
 const clientOptions = {
 	options: { debug: true, messagesLogLevel: "info" },
@@ -26,22 +57,12 @@ client.on('message', (channel, userstate, message, self) => {
         userstate,
     });
 
+    message = message.toLowerCase();
+
     if (self) return;
     
-    // if (userstate.username === 'faithlessfew') {
-    //     console.log('master does as he pleases').
-    //     return;
-    // }
+	if (message === '!hello') hello(client, channel, userstate)
 
-	if (message.toLowerCase() === '!hello') {
-		client.say(channel, `@${userstate.username} world!`);
-    }
-    
-    const shouldDeleteMessage = BLOCKED_WORDS.some(blockedWord => message.toLowerCase().includes(blockedWord.toLowerCase()));
-
-    if (shouldDeleteMessage) {
-        client.say(channel, `Sorry @${userstate['display-name']}, your message contained content not consistent with this stream's rules, and has been deleted!`);
-
-        client.deletemessage(channel, userstate.id).then(() => console.log('messaged deleted')).catch(err => console.log('problem deleting message', err));
-    }
+        // client.deletemessage(channel, userstate.id).then(() => console.log('messaged deleted')).catch(err => console.log('problem deleting message', err));
+    // if (message === '!bang') bang(client, channel, userstate)
 });
